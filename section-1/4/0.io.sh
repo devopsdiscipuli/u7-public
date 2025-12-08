9bb1f39849b87f943048d4a9142638efa7eee98e
devopsdiscipuli <devopsdiscipuli@gmail.com>
2025-12-08 09:06:59 +0530

    +

---
+++ section-1/4/0.io.sh
@@ -0,0 +1,33 @@
+itzamna@DESKTOP-R0QENLJ MINGW64 ~/Documents/GitHub/u7-public/section-1/4 (main)
+$ docker run hello-world
+Unable to find image 'hello-world:latest' locally
+latest: Pulling from library/hello-world
+17eec7bbc9d7: Pull complete
+ea52d2000f90: Download complete
+Digest: sha256:f7931603f70e13dbd844253370742c4fc4202d290c80442b2e68706d8f33ce26
+Status: Downloaded newer image for hello-world:latest
+
+Hello from Docker!
+This message shows that your installation appears to be working correctly.
+
+To generate this message, Docker took the following steps:
+ 1. The Docker client contacted the Docker daemon.
+ 2. The Docker daemon pulled the "hello-world" image from the Docker Hub.
+    (amd64)
+ 3. The Docker daemon created a new container from that image which runs the
+    executable that produces the output you are currently reading.
+ 4. The Docker daemon streamed that output to the Docker client, which sent it
+    to your terminal.
+
+To try something more ambitious, you can run an Ubuntu container with:
+ $ docker run -it ubuntu bash
+
+Share images, automate workflows, and more with a free Docker ID:
+ https://hub.docker.com/
+
+For more examples and ideas, visit:
+ https://docs.docker.com/get-started/
+
+
+itzamna@DESKTOP-R0QENLJ MINGW64 ~/Documents/GitHub/u7-public/section-1/4 (main)
+$ 
