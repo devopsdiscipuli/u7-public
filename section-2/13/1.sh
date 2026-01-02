246585f0131de6423bce7b4752d35c6fecc55c02
devopsdiscipuli <devopsdiscipuli@gmail.com>
2026-01-02 09:14:24 +0530

    +

---
+++ section-2/13/1.sh
@@ -0,0 +1,21 @@
+itzamna@DESKTOP-R0QENLJ MINGW64 ~/Documents/GitHub/u7-public/section-2/13 (main)
+$ docker images
+                                                                                                  i Info →   U  In Use
+IMAGE                ID             DISK USAGE   CONTENT SIZE   EXTRA
+busybox:latest       d80cd694d3e9       6.78MB         2.22MB    U 
+hello-world:latest   f7931603f70e       25.9kB         9.52kB    U 
+mysql:latest         fe036967257b       1.29GB          293MB    U 
+redis:latest         47200b041382        205MB         55.4MB    U 
+
+itzamna@DESKTOP-R0QENLJ MINGW64 ~/Documents/GitHub/u7-public/section-2/13 (main)
+$ docker images -a
+                                                                                                  i Info →   U  In Use
+IMAGE                ID             DISK USAGE   CONTENT SIZE   EXTRA
+busybox:latest       d80cd694d3e9       6.78MB         2.22MB    U 
+hello-world:latest   f7931603f70e       25.9kB         9.52kB    U 
+mysql:latest         fe036967257b       1.29GB          293MB    U 
+redis:latest         47200b041382        205MB         55.4MB    U 
+
+itzamna@DESKTOP-R0QENLJ MINGW64 ~/Documents/GitHub/u7-public/section-2/13 (main)
+$
+
